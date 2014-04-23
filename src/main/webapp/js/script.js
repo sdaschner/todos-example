ba86fbd67e4b6021ed2326db62c6077bde3ce3ed
Sebastian Daschner <git@sebastian-daschner.de>
2014-04-23 17:34:09 +0200

    added apache licence header

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,10 +1,26 @@
+/*
+ * Copyright (C) 2014 Sebastian Daschner, sebastian-daschner.de
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 de = {};
 de.sd = {};
 de.sd.todos = {
 
     filterQuery: null,
-    tasksUrl: '/resources/tasks/',
-    filterUrl: '/resources/filters/',
+    tasksUrl: '/todos/resources/tasks/',
+    filterUrl: '/todos/resources/filters/',
 
     init: function() {
         de.sd.todos.bindEvents();
